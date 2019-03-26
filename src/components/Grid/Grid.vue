335468b0ac9b2996e34697b73c3069c4924972fa
cassandraGoose <cassandra.torske@gmail.com>
2019-03-26 13:06:05 -0600

    fix guideline bug

--- src/components/Grid/Grid.vue
+++ src/components/Grid/Grid.vue
@@ -331,6 +331,12 @@ export default {
       this.points = [];
       this.draw();
     },
+    points() {
+      if (this.points.length === 0) {
+        this.eraseGuidelines();
+      }
+      this.drawPoints();
+    },
     transform(newTransform, lastTransform) {
       // hide polygon names if zoomed out enough
       if (newTransform.k < 0.5) {
